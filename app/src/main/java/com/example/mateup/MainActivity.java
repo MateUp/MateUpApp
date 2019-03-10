7207696766672472d9cedc1e8b640853546555b3
MateUp <appmateup@gmail.com>
2019-03-10 16:28:19 +0100

    Prepravljeno videos u posts

--- app/src/main/java/com/example/mateup/MainActivity.java
+++ app/src/main/java/com/example/mateup/MainActivity.java
@@ -4,14 +4,8 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -20,12 +14,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
-
-import com.example.mateup.FragmentsCardsForProfile.BiographyFragment;
-import com.example.mateup.FragmentsCardsForProfile.PartnersFragment;
-import com.example.mateup.FragmentsCardsForProfile.VideosFragment;
-
-import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
