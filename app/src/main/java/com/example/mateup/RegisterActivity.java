6b070595e4ed003125eca06a5587afbe7f47e33d
MateUp <appmateup@gmail.com>
2019-03-17 10:39:40 -0700

    Zadnja proba

--- app/src/main/java/com/example/mateup/RegisterActivity.java
+++ app/src/main/java/com/example/mateup/RegisterActivity.java
@@ -1,11 +1,18 @@
 package com.example.mateup;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.example.mateup.services.RestClient;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class RegisterActivity extends AppCompatActivity {
     private Button RegisterButton;
@@ -39,7 +46,48 @@ public class RegisterActivity extends AppCompatActivity {
 
     }
     public void openStoryLineActivity(){
-        Intent openStoryLine = new Intent(this, MainActivity.class);
-        startActivity(openStoryLine);
+//        Intent openStoryLine = new Intent(this, MainActivity.class);
+//        startActivity(openStoryLine);
+
+//        String data = new
+
+
+        final JSONObject user = new JSONObject();
+        try {
+            user.put("email", EmailRegister.getText());
+            user.put("firstName", FirstNameRegister.getText());
+            user.put("lastName", LastNameRegister.getText());
+            user.put("password", passwordRegister.getText());
+            user.put("country",CountryRegister.getText());
+            user.put("profesion", RCH.getText());
+        } catch (JSONException e) {
+            Log.e("Filippppp", "FILIP_ERR 1");
+            e.printStackTrace();
+        }
+
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    RestClient rc = new RestClient("/users");
+                    String response = rc.executePost(user.toString());
+
+                    Log.i("Server response", response);
+                    if (response != null) {
+                        JSONObject res = new JSONObject(response);
+                        res.get("token");
+                    }
+                } catch (Exception e) {
+                    Log.e("Filippppp", e.getMessage());
+//                    Log.e("Filippppp", e.toString());
+                    Log.v("Filipppp", "Verbos");
+                    e.printStackTrace();
+                }
+            }
+        });
+        t.start();
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/mateup/services/RestClient.java
@@ -0,0 +1,137 @@
+package com.example.mateup.services;
+
+/*Copyright 2014 Bhavit Singh Sengar
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.*/
+
+
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHeaders;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.util.Log;
+import android.widget.Toast;
+
+public class RestClient {
+
+
+    JSONObject data = new JSONObject();
+    String url;
+    String headerName;
+    String headerValue;
+
+
+    public RestClient(String s){
+
+        url = "https://367d4f76.ngrok.io/api" + s;
+
+        Header header1 = new BasicHeader(HttpHeaders.CONTENT_TYPE, "application/json");
+
+        //
+//        if (ako ima token) {
+//            Header header1 = new BasicHeader(HttpHeaders.AUTHORIZATION, Dodaj token);
+//        }
+    }
+
+
+    public void addHeader(String name, String value){
+
+        headerName = name;
+        headerValue = value;
+
+    }
+
+    public void addParam(String key, String value){
+
+        try {
+            data.put(key, value);
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+    public String executePost(String data){  // If you want to use post method to hit server
+
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpPost httpPost = new HttpPost(url);
+        httpPost.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
+        HttpResponse response = null;
+        String result = null;
+        try {
+            Log.i("Filip", this.url);
+            StringEntity entity = new StringEntity(data, HTTP.UTF_8);
+            httpPost.setEntity(entity);
+            response = httpClient.execute(httpPost);
+            HttpEntity entity1 = response.getEntity();
+            result = EntityUtils.toString(entity1);
+            return result;
+            //Toast.makeText(MainPage.this, result, Toast.LENGTH_LONG).show();
+        } catch (UnsupportedEncodingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            Log.e("Filip", "E1");
+        } catch (ClientProtocolException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            Log.e("Filip", "E2");
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            Log.e("Filip", e.getMessage());
+        }
+        return result;
+
+
+
+    }
+
+    public String executeGet(){ //If you want to use get method to hit server
+
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpGet httpget = new HttpGet(url);
+        String result = null;
+        ResponseHandler<String> responseHandler = new BasicResponseHandler();
+        try {
+            result = httpClient.execute(httpget, responseHandler);
+        } catch (ClientProtocolException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+}
+
