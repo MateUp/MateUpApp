8231f800931ce7f56a27ec61be60c33727f93f98
Nemanja <nscepanovic11@gmail.com>
2019-04-14 16:38:40 +0200

    File upload

--- app/src/main/java/com/example/mateup/AddPostActivity.java
+++ app/src/main/java/com/example/mateup/AddPostActivity.java
@@ -2,6 +2,7 @@ package com.example.mateup;
 
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
@@ -12,22 +13,38 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v4.content.CursorLoader;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.mbms.FileServiceInfo;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.Toast;
 
+
+import com.example.mateup.models.FileUpload;
 import com.example.mateup.services.RestClient;
+import com.ipaulpro.afilechooser.utils.FileUtils;
 
 import org.json.JSONObject;
 
+import java.io.File;
+
 import java.io.IOException;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
+
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class AddPostActivity extends AppCompatActivity {
 
@@ -35,7 +52,7 @@ public class AddPostActivity extends AppCompatActivity {
     public ImageView postReview;
     public EditText description;
     public Button postBtn;
-    private static final int SELECT_PICTURE = 0;
+    private Uri path;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,77 +63,27 @@ public class AddPostActivity extends AppCompatActivity {
         description = (EditText) findViewById(R.id.description);
         postBtn = (Button) findViewById(R.id.post_btn);
 
+        selectImage();
+
         postBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getIdAndUpload();
+                createPostAndUploadImage();
             }
         });
-
-        GettingFromGallery();
-
-
     }
 
 
-
-    private void GettingFromGallery() {
-
-
-
-
-        Intent pickPhoto = new Intent(Intent.ACTION_PICK,
-                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        startActivityForResult(pickPhoto, SELECT_PICTURE);
-    }
-
-
-     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-         super.onActivityResult(requestCode, resultCode, data);
-
-         switch (requestCode) {
-             case SELECT_PICTURE:
-
-                 if (resultCode == Activity.RESULT_OK) {
-                     Uri selectedImage = data.getData();
-                     String[] filePathColumn = { MediaStore.Images.Media.DATA };
-
-                     Cursor cursor = getContentResolver().query(selectedImage,
-                             filePathColumn, null, null, null);
-                     cursor.moveToFirst();
-
-                     int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-                     String picturePath = cursor.getString(columnIndex);
-                     cursor.close();
-
-
-                     if (bm != null && bm.isRecycled()) {
-
-                         bm = null;
-                     }
-                     bm = BitmapFactory.decodeFile(picturePath);
-                     postReview.setImageBitmap(bm);
-                     postReview.setImageURI(selectedImage);
-
-                 }
-         }
-
-
-     }
-
-    private void getIdAndUpload() {
-        Thread t = new Thread(new Runnable() {
+    private  void createPostAndUploadImage() {
+        AsyncTask.execute(new Runnable() {
             @Override
             public void run() {
-
-
-
                 try {
                     JSONObject postDescription = new JSONObject();
-                    postDescription.put("content",description.getText());
+                    postDescription.put("content", description.getText());
                     RestClient rc = new RestClient("/posts");
                     String response = rc.executePost(postDescription.toString());
-                    Log.i("content",response);
+                    Log.i("content", response);
 
 
                     if (response != null) {
@@ -124,100 +91,83 @@ public class AddPostActivity extends AppCompatActivity {
                         String postId = res.getString("_id");
                         Log.i("post", postId);
 
-
-                        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(AddPostActivity.this);
-                        SharedPreferences.Editor editor = sharedPref.edit();
-                        editor.putString("_id", postId);
-
+                        uploadImage(postId);
                     }
 
 
                 } catch (Exception e) {
-                    Log.e("Filippppp", e.getMessage());
-                    Log.e("Filippppp", e.toString());
-                    Log.v("Filipppp", "Verbos");
                     e.printStackTrace();
 
                 }
-
-
             }
-        });t.start();upload();
-
-
-
-
-
-
+        });
     }
 
+    private void selectImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 1);
+    }
 
+    private void uploadImage(final String postId) {
 
-    private void upload() {
+        Context applicationContext = LoginActivity.getContextOfApplication();
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(applicationContext);
+        final String token = pref.getString("token","not found");
 
-        AsyncTask.execute(new Runnable() {
+        File fileObj = FileUtils.getFile(AddPostActivity.this, path);
+
+        RequestBody postPart = RequestBody.create(MultipartBody.FORM, postId);
+        RequestBody filePart = RequestBody.create(
+            MediaType.parse(getContentResolver().getType(path)),
+            fileObj
+        );
+
+        MultipartBody.Part file = MultipartBody.Part.createFormData("file", "file", filePart);
+
+        Retrofit.Builder builder = new Retrofit.Builder()
+                .baseUrl("https://mateup.nstechlabs.com/")
+                .addConverterFactory(GsonConverterFactory.create());
+
+        Retrofit retrofit = builder.build();
+
+        FileUpload fileUpload = retrofit.create(FileUpload.class);
+
+        Call<ResponseBody> call = fileUpload.uploadImage(token, postPart, file);
+        call.enqueue(new Callback<ResponseBody>() {
             @Override
-            public void run() {
-                SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(AddPostActivity.this);
-                String postId = pref.getString("_id", "not found");
-                try {
-
-                    String a = String.valueOf(description.getText());
-
-
-                    // URL url = new URL(
-                    String URL_BASE = "https://mateup.nstechlabs.com/api/files";
-
-                    String url1 = String
-                            .format(URL_BASE
-                                            + "description_comment_=%s&member_id_comment_=%s&product_id_comment_=%s&store_id_comment_=%s",
-                                    a, 1, 191, 55);
-
-                    URL url = new URL(url1);
-
-                    HttpURLConnection c = (HttpURLConnection) url.openConnection();
-                    // this HTTP request will involve input
-                    c.addRequestProperty("app_key", "rg946A");
-                    c.addRequestProperty("post_id",postId);
-                    c.addRequestProperty("description", description.getText().toString());
-                    // c.addRequestProperty("categories", SelcectItem);
-                    c.setDoInput(true);
-                    // should be PUT or POST to follow convention
-                    c.setRequestMethod("POST");
-                    System.out.println(c.getRequestProperties().toString());
-                    // this HTTP request will involve output
-                    c.setDoOutput(true);
-                    // open the HTTP connection
-                    c.connect();
-                    OutputStream output = c.getOutputStream();
-                    // compress and write the image to the output stream
-                    bm.compress(Bitmap.CompressFormat.JPEG, 100, output);
-
-                    output.close();
-
-                    System.out.println(c.getResponseMessage());
-                    if (c.getResponseMessage().equalsIgnoreCase("OK")) {
-
-                        Toast.makeText(
-                                getApplicationContext(),
-                                "Picture Upload Successfully..\n"
-                                        + "Picture will be available after admin approval",
-                                Toast.LENGTH_LONG).show();
-                    }
-
-                } catch (IOException e) {
-                    // log error
-                    e.printStackTrace();
-                    Log.e("ImageUploader", "Error uploading image", e);
-                }
-
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+               // INFO: Slika je uspesno aplodovana
             }
-            });
-        }
 
-
-
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                // INFO: Error
+                t.printStackTrace();
+            }
+        });
     }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null) {
+            path = data.getData();
+
+            try {
+                bm = MediaStore.Images.Media.getBitmap(getContentResolver(), path);
+                postReview.setImageBitmap(bm);
+                postReview.setVisibility(View.VISIBLE);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
 
 
 
